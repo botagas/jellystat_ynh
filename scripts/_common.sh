91e3dcfe051b1606cb89ffe3bdbcff17158bce72
Patrick <49989644+botagas@users.noreply.github.com>
2025-01-06 20:23:05 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,8 +10,8 @@ app=jellystat
 
 # Function to find an unused port (used in the install script)
 ynh_find_port() {
-    local port=3000  # Starting port, adjust as needed
-    while netstat -tuln | grep -q ":$port"; do
+    local port=3000  # Starting port
+    while ss -tuln | grep -q ":$port"; do
         port=$((port + 1))
     done
     echo $port
