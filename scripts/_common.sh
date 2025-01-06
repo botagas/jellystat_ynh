45870ed906e04c7698b16f75f1a0a06241c92c1e
Patrick <49989644+botagas@users.noreply.github.com>
2025-01-06 20:21:00 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,16 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+app=jellystat
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+# Function to find an unused port (used in the install script)
+ynh_find_port() {
+    local port=3000  # Starting port, adjust as needed
+    while netstat -tuln | grep -q ":$port"; do
+        port=$((port + 1))
+    done
+    echo $port
+}
