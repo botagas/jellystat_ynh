61b04c505ae83225e9df2c5080be174b0245a78e
Patrick <49989644+botagas@users.noreply.github.com>
2025-01-07 02:43:45 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@ app=jellystat
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-
+nodejs_version=20
 # Function to find an unused port (used in the install script)
 ynh_find_port() {
     local port=3000  # Starting port
