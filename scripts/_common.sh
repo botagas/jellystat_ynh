9cf071f7e6027b2e6888570d5bfe4e7b0b0066bb
Patrick <49989644+botagas@users.noreply.github.com>
2025-01-07 10:35:20 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 app=jellystat
 nodejs_version=20
-
+app_port=3000
 # Directories
 install_dir="/var/www/$app"
 backup_dir="/home/yunohost.app/$app/backups"
@@ -15,10 +15,10 @@ data_dir="/home/yunohost.app/$app"
 # PERSONAL HELPERS
 #=================================================
 # Function to find an unused port (used in the install script)
-ynh_find_port() {
-    local port=3000  # Starting port
-    while ss -tuln | grep -q ":$port"; do
-        port=$((port + 1))
-    done
-    echo $port
-}
+# ynh_find_port() {
+#     local port=3000  # Starting port
+#     while ss -tuln | grep -q ":$port"; do
+#         port=$((port + 1))
+#     done
+#     echo $port
+# }
