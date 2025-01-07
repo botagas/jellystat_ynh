6a16229aeef8e3f2ee432b5059d9909cd72ec980
Patrick <49989644+botagas@users.noreply.github.com>
2025-01-07 17:19:26 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,7 +15,7 @@ jwt_secret=OAMkowavpFTdCaX6VXmiIQrS
 # Directories
 install_dir="/var/www/$app"
 backup_dir="/home/yunohost.app/$app/backups"
-backup_data="/var/www/$app/backend/backup-data"
+backup_data="/var/www/$app/backup-data"
 data_dir="/home/yunohost.app/$app"
 #=================================================
 # PERSONAL HELPERS
