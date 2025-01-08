b67de5d2a29dc283496271b41b20d5ff4f59f221
Patrick <49989644+botagas@users.noreply.github.com>
2025-01-08 04:24:59 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,11 +7,8 @@
 app=jellystat
 nodejs_version=20
 postgres_user="jfstat"
-postgres_password=R5iWxG4MWe9ddUji4TXqsysR
 postgres_db="jfstat"
 js_user="admin"
-js_password=MwhAixLDrwWHLn6TTUCLqAJc
-jwt_secret=OAMkowavpFTdCaX6VXmiIQrS
 
 # Directories
 install_dir="/var/www/$app"
