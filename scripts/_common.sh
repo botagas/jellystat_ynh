d578b8b60c014fc2b4fc86b7efbc486aeea599e9
Patrick <49989644+botagas@users.noreply.github.com>
2025-01-09 13:47:45 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,6 +6,8 @@
 
 app=jellystat
 nodejs_version=20
+backup_dir="/home/yunohost.app/jellystat/backups"
+backup_data="/var/www/jellystat/backend/backup-data"
 
 #=================================================
 # PERSONAL HELPERS
